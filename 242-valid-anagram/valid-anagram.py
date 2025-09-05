2a71276f84f04f649c4d4c82f9e61f073dbf7445
ahmad-200 <ahmedhussan.568@gmail.com>
2025-09-05 15:42:46 +0500

    Time: 7 ms (97.61%) | Memory: 12.6 MB (68.77%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,7 +5,19 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if sorted(s)==sorted(t):
-            return True
+        dic_s={}
+        dic_t={}
+        for i in s:
+            if i in dic_s:
+                dic_s[i]+=1
+            else:
+                dic_s[i]=1
+        for i in t:
+            if i in dic_t:
+                dic_t[i]+=1
+            else:
+                dic_t[i]=1
+        if dic_t==dic_s:
+            return True 
         else:
             return False
